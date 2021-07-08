2c8efb9104d5ea4808271b5e80f76618ba819f86
Jason Redfoot <jredfoot@asu.edu>
2021-07-08 07:25:37 -0400

    All of Module 2 code along stater files and the skill builder starter file.

---
+++ Module_2/WD2-Module-2-00-Skill-Builder-Fancy-Carpet-Calcu-jredfoot/script.js
@@ -0,0 +1,46 @@
+const widthElement = document.getElementById('width');
+const lengthElement = document.getElementById('length');
+const areaElement = document.getElementById('area-box');
+
+function showCarpetArea(width, length, area) {
+  let widthPx;
+  let lengthPx;
+  
+  if (width > length) {
+    widthPx = 300;
+    lengthPx = Math.max(20, (length / width) * 300); 
+  } else {
+    widthPx = (width / length) * 300;   
+    lengthPx = 300;
+  }
+
+  areaElement.style.opacity = 1;
+  window.setTimeout(()=> {
+    widthElement.innerHTML = width;
+    widthElement.style.opacity = 1;
+    areaElement.style.width = widthPx + "px";
+    window.setTimeout(()=> {
+      lengthElement.innerHTML = length;
+      lengthElement.style.opacity = 1;
+      areaElement.style.height = lengthPx + "px";
+      window.setTimeout(()=> {
+        areaElement.style.fontSize = Math.min(lengthPx, 60) + "px";
+        areaElement.style.lineHeight = lengthPx + "px";
+        areaElement.innerHTML = area;
+      }, 700);
+    }, 700);
+  }, 700);
+}
+
+/* 
+The showCarpetArea function takes three values:
+	1. The width of the carpet
+	2. The height of the carpet
+	3. The area of the carpet
+
+Try passing different values to showCarpetArea() 
+to see how it changes the visualization!
+*/
+
+// Your code here
+showCarpetArea(1, 1, 1);
