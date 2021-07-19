a331433fdec66421eee5a2a616d4de909c91ca5d
Jason Redfoot <jredfoot@asu.edu>
2021-07-19 07:11:18 -0400

    Adding Thursdays code along and all of Module 5s video files.

--- Module_4/WD2-Module-4-00-Skill-Builder-Words-of-Encourage-jredfoot/script.js
+++ Module_4/WD2-Module-4-00-Skill-Builder-Words-of-Encourage-jredfoot/script.js
@@ -26,8 +26,9 @@ function encourage() {
   //get the name and words of encouragement.
   let name = document.getElementById('name').value;
   let encouragement = document.getElementById('encouragement').value;
+  let postScript = document.getElementById('post-script').value;
   // Combine those into some text.
-  let message = `Hello ${name}, I have something I would like to tell you. ${encouragement} Have a nice day!`;
+  let message = `Hello ${name}, I have something I would like to tell you. ${encouragement} Have a nice day! P.S. ${postScript}`;
   // Render text to the user.
   renderTTS(message)
 }
---
+++ Module_5/function-mob-practice-starter/script.js
@@ -0,0 +1,71 @@
+
+// #1. capslock() - takes in one string and returns the same string with all capital letters
+function capslock(str) {
+  // code here
+  return str.toUpperCase();
+}
+// UNCOMMENT TO TEST: 
+console.log(capslock("javascript is bae"));
+
+console.log('--------------------------------------')
+
+//#2. dogYears() - takes in a number and returns that number multiplied by 7
+
+
+// Define the function here!
+function dogYears(age) {
+  let dogAge = age * 7;
+  return dogAge;
+}
+
+// UNCOMMENT TO TEST: 
+console.log(`You are ${dogYears(30)} in dog years!`);
+console.log('--------------------------------------');
+//#3. randomNum() - returns a random number between 1 and 10
+//HINT: use Math.random
+//HINT2: you'll need to mutiply the result by 10
+//LEVEL-UP: use Math.floor or Math.ceil to round the result to a whole number
+
+// Define the function here!
+
+function randomNum() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// UNCOMMENT TO TEST:
+console.log(randomNum());
+console.log(randomNum());
+console.log(randomNum());
+
+console.log('--------------------------------------');
+
+//#4. asciiArt() - no parameters; prints out a piece of ascii art when run
+// HINT: you can create multi-line strings if you use backticks instead of quotations marks (if there are backticks in the art itself, you'll need to replace those with single quotes aka apostrophes)
+// HINT #2: notice the instructions say to *print* the result (as opposed to return)
+
+// Define the function here!
+function asciiArt() {
+  console.log(`
+    __    __
+o-''))_____\\
+"--__/ * * * )
+c_c__/-c____/
+  `);
+}
+
+// UNCOMMENT TO TEST: 
+asciiArt()
+
+console.log('--------------------------------------')
+
+//BONUS: whatMonthIsIt() - returns a number corresponding to the current month (July = 7)
+//HINT1: first look up how to get the current date from the Date javascript object
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date
+//HINT2: store the current date in a variable called "now", then call .getMonth() on that variable and return the result
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getMonth
+//HINT3: If you find the numbers are off by one, make sure to adjust accordingly!
+
+// Define the function here!
+
+// UNCOMMENT TO TEST: 
+// console.log(whatMonthIsIt())
