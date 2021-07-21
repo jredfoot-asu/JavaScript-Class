bacb57ff384a3258cba1eeff0add0eaf8878442b
Jason Redfoot <jredfoot@asu.edu>
2021-07-21 07:24:22 -0400

    Uploading the video code alongs

---
+++ Module_6/WD2-Module-7-Midterm-00-Project-Game-1-Magic8Ba-jredfoot/script.js
@@ -0,0 +1,37 @@
+
+/* 
+   Welcome to the Magic 8-ball Project.  Complete the 
+   tasks outlined by the comments in this file.
+
+   The following functions are available to you 
+   (you can just call them):
+
+   // This will hide the '8' (if shown) and 
+   // show the triangle (if hidden).  It will
+   // set the text on the triangle to the
+   // string 'answerText'.
+   function makeAnswerAppear(answerText)
+*/
+
+/* Returns a random integer in the range 'min' through 'max' inclusive. 
+
+   This can be taken directly from MDN documentation: 
+     https://tinyurl.com/3jkxa8h3
+
+*/
+function getRandomIntInclusive(min, max) {
+}
+
+/* Write a function that handles the magic 8-ball being clicked. Here are the steps:
+
+   - Create an array containing at least 5 possible answers as strings.
+   - Generate a random array index by calling 
+       getRandomIntInclusive function.
+   - Call 'makeAnswerAppear' using the random
+       answer you selected.
+   - (Level-up) Prevent your code from selecting the 
+       same answer multiple times in a row 
+       (loops could be required).
+*/
+function answerQuestion() {
+}
